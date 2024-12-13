65a4dad95e79a3cff05e182288d731863a577601
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-12-13 19:42:55 +0530

    Profile pic uploading feat. added  --fixing -03

--- internals/middlewares/CorsMiddlewares.go
+++ internals/middlewares/CorsMiddlewares.go
@@ -18,11 +18,14 @@ func CoopMiddleware(next http.Handler) http.Handler {
 }
 
 func HandleOptionsMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == http.MethodOptions {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Origin", "https://try-your-gyan.vercel.app") // Update to specific origin if needed
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
