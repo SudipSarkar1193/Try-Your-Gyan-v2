8f709edae464278efe1a7809ad0fee14a1aa63d4
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-12-14 13:23:49 +0530

    Profile pic uploading feat. added  --fixing 24

--- internals/middlewares/CorsMiddlewares.go
+++ internals/middlewares/CorsMiddlewares.go
@@ -66,11 +66,11 @@ func HandleOptionsMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			log.Println("Handled r.Method == http.MethodOptions ")
+			log.Println("Handled 'r.Method == http.MethodOptions' block ")
 			w.WriteHeader(http.StatusOK)
-			return
+
 		}
-		log.Println()
+		log.Println("Reached out of if 'r.Method == http.MethodOptions' block")
 
 		next.ServeHTTP(w, r)
 	})
