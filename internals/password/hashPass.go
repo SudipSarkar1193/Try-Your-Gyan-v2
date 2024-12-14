78da6c5e16306b2a5d4d7d497ddd1ae002df615f
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-12-14 18:44:01 +0530

    Profile pic uploading feat. added  --testing 003

--- internals/password/hashPass.go
+++ internals/password/hashPass.go
@@ -1,18 +1,37 @@
 package password
 
 import (
+	"fmt"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+func HashPassword(password interface{}) (string, error) {
+	// Type assertion to ensure `password` is of type `string`
+	strPassword, ok := password.(string)
+	if !ok {
+		return "", fmt.Errorf("invalid type for password: expected string")
+	}
+
+	// Generate a hashed password with bcrypt
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(strPassword), 14)
 	if err != nil {
 		return "", err
 	}
-	return string(hashedPassword), err
+
+	// Return the hashed password as a string
+	return string(hashedPassword), nil
 }
 
-func CheckPassword(password string, hash string) (bool, error) {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil, err
+
+func CheckPassword(password string, hash interface{}) (bool, error) {
+    // Type assertion to ensure `hash` is of type `string`
+    strHash, ok := hash.(string)
+    if !ok {
+        return false, fmt.Errorf("invalid type for hash: expected string")
+    }
+
+    // Compare the hash and password
+    err := bcrypt.CompareHashAndPassword([]byte(strHash), []byte(password))
+    return err == nil, err
 }
